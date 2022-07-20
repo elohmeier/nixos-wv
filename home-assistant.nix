c89c6329065c664c4878b00f1e4b2a5ac98503d8
Enno Richter <enno@nerdworks.de>
2022-07-20 07:15:06 +0200

    hass: configure ip bans

--- home-assistant.nix
+++ home-assistant.nix
@@ -32,10 +32,12 @@
       };
 
       http = {
-        server_host = "127.0.0.1";
+        server_host = [ "127.0.0.1" "::1" ];
         server_port = 8123;
         use_x_forwarded_for = true;
         trusted_proxies = [ "127.0.0.1" "::1" ];
+        ip_ban_enabled = true;
+        login_attempts_threshold = 5;
       };
     };
   };
