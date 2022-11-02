0566b825b67fc2075c0898634a7ce9a6b77c1b93
Enno Richter <enno@nerdworks.de>
2022-11-02 13:58:48 +0100

    flake: configure nixinate

--- flake.nix
+++ flake.nix
@@ -1,13 +1,22 @@
 {
   description = "nixos-wv";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+  inputs = {
+    nixinate.inputs.nixpkgs.follows = "nixpkgs";
+    nixinate.url = "github:elohmeier/nixinate";
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+  };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixinate, nixpkgs }: {
+    apps = nixinate.nixinate.aarch64-darwin self;
+
     nixosConfigurations = {
       hetznervm = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [ ./hetznervm ];
+        modules = [
+          ./hetznervm
+          { _module.args.nixinate = { host = "def.lf42.de"; sshUser = "root"; buildOn = "remote"; }; }
+        ];
       };
 
       rpi3-klipper = nixpkgs.lib.nixosSystem {
